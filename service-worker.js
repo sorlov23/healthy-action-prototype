8c4e4b3c6e15529bce0c87d3e602f095518e0abe
sorlov23 <my@orlov23.ru>
2026-08-30 18:18:56 +0300

    Refresh PWA cache for swipe-to-dismiss

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='healthy-action-pwa-v5';
+const CACHE='healthy-action-pwa-v6';
 const ASSETS=['./pwa.html','./v07.html','./food-catalog.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
