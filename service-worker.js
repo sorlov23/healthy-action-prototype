41ea898394328975c6606af4c2df9953a4f537d0
sorlov23 <my@orlov23.ru>
2026-08-30 17:08:48 +0300

    Refresh PWA cache for polished onboarding

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='healthy-action-pwa-v2';
+const CACHE='healthy-action-pwa-v3';
 const ASSETS=['./pwa.html','./v07.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
