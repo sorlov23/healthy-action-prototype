26ab586c7f43a070f8bd99bfba8a4378c492d59d
sorlov23 <my@orlov23.ru>
2026-08-30 20:19:08 +0300

    Refresh PWA cache for v0.9.2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='healthy-action-pwa-v11';
-const ASSETS=['./pwa.html','./v07.html?rev=11','./food-catalog.js?rev=11','./pwa-enhancements.js?rev=11','./ui-polish.js?rev=11','./manifest.webmanifest','./icon.svg'];
+const CACHE='healthy-action-pwa-v12';
+const ASSETS=['./pwa.html','./v07.html?rev=12','./food-catalog.js?rev=12','./pwa-enhancements.js?rev=12','./ui-polish.js?rev=12','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./pwa.html'))));});
