5754af9f62a39c6623be4d864116184e857c6669
sorlov23 <my@orlov23.ru>
2026-09-09 03:47:49 +0300

    Cache Rinlo functional MVP assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rinlo-pwa-v29';
+const CACHE='rinlo-pwa-v30';
 const ASSETS=[
   './pwa.html',
   './v07.html?rev=24',
@@ -19,10 +19,11 @@ const ASSETS=[
   './rinlo-system-v01.js?rev=24',
   './rinlo-core-ui-v1.js?rev=25',
   './rinlo-scale-pass-v1.js?rev=29',
-  './rinlo-wordmark.js?rev=29',
-  './rinlo-logo.svg?rev=29',
+  './rinlo-functional-v1.js?rev=30',
+  './rinlo-wordmark.js?rev=30',
+  './rinlo-logo.svg?rev=30',
   './manifest.webmanifest',
-  './icon.svg?rev=29'
+  './icon.svg?rev=30'
 ];
 
 self.addEventListener('install',event=>event.waitUntil(
