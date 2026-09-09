5d7fb1cbadf0534d62456f4885bfa9f83cbf5d28
sorlov23 <my@orlov23.ru>
2026-09-09 11:08:57 +0300

    bump PWA cache for server sync layer

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rinlo-pwa-v31';
+const CACHE='rinlo-pwa-v32';
 const ASSETS=[
   './pwa.html',
   './v07.html?rev=24',
@@ -21,6 +21,7 @@ const ASSETS=[
   './rinlo-scale-pass-v1.js?rev=29',
   './rinlo-functional-v1.js?rev=30',
   './rinlo-stability-v1.js?rev=31',
+  './rinlo-server-sync-v1.js?rev=32',
   './rinlo-wordmark.js?rev=30',
   './rinlo-logo.svg?rev=31',
   './manifest.webmanifest',
