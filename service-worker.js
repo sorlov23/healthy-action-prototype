b3770bce0b873f9234338dad6bde436c9f9886b2
sorlov23 <my@orlov23.ru>
2026-09-11 18:20:49 +0300

    chore: bump Rinlo PWA cache to v45

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rinlo-pwa-v44';
+const CACHE='rinlo-pwa-v45';
 const ASSETS=[
   './pwa.html',
   './v07.html?rev=24',
@@ -18,7 +18,7 @@ const ASSETS=[
   './rinlo-onboarding-v01.js?rev=24',
   './rinlo-plan-v01.js?rev=37',
   './rinlo-insights-v01.js?rev=24',
-  './rinlo-profile-v01.js?rev=24',
+  './rinlo-profile-v01.js?rev=45',
   './rinlo-system-v01.js?rev=35',
   './rinlo-core-ui-v1.js?rev=25',
   './rinlo-scale-pass-v1.js?rev=29',
