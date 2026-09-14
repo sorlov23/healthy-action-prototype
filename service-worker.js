99cce819e14ac0c0acae6958dac78741de0e020c
sorlov23 <my@orlov23.ru>
2026-09-14 03:30:52 +0300

    Align Today v3 asset revisions

--- service-worker.js
+++ service-worker.js
@@ -38,9 +38,9 @@ const ASSETS=[
   './rinlo-wordmark.js?rev=30',
   './rinlo-settings-bridge-v1.js?rev=34',
   './rinlo-copy-pass-v1.js?rev=1',
-  './rinlo-today-v3-observer-guard.js?rev=1',
+  './rinlo-today-v3-observer-guard.js?rev=2',
   './rinlo-today-v3.js?rev=1',
-  './rinlo-today-v3-compat.js?rev=1',
+  './rinlo-today-v3-compat.js?rev=2',
   './rinlo-logo.svg?rev=31',
   './manifest.webmanifest',
   './icon.svg?rev=31'
